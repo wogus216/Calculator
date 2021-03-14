4e9dd474785284e212dba23d446cd1d1870cd681
wogus <wogus@DESKTOP-KNV88FV>
2021-03-14 11:23:20 +0900

    메소드 최종 구현!!

--- src/Calculator.java
+++ src/Calculator.java
@@ -1,43 +1,70 @@
 import java.util.Scanner;
 
 public class Calculator {
- public static void main(String[] args) {
-	 Scanner sc= new Scanner(System.in);
-	 System.out.println("첫번째 숫자를 입력해주세요");
-	 int num1= sc.nextInt();
-	 System.out.println(num1);
+	static int getFirstValue(Scanner sc) {
+		System.out.println("첫번 째 숫자를 입력해주세요");
+		int first=sc.nextInt();
+		System.out.println(first);
+		return first;
+	}
 	
-	 int result=num1;
-	 while(true) {
+	static String getSymbol(Scanner sc) {
+		System.out.println("사칙연산 기호를 입력해주세요");
+		String symbol=sc.next();
+		System.out.println(symbol);
+		return symbol;
+	}
+	
+	static int getSecondValut(Scanner sc) {
+		System.out.println("두번 째 숫자를 입력해주세요");
+		int second=sc.nextInt();
+		System.out.println(second);
+		return second;
+	}
+	
+	static int calculate(int first, String symbol, int second) {
+		int result=0;
+		if(symbol.equals("+")) {
+			result=first+second;
+			System.out.println("덧셈의 결과 :"+result);
+		} else if(symbol.equals("-")) {
+			result=first-second;
+			System.out.println("뺄셈의 결과 : "+result);
+		} else if(symbol.equals("*")) {
+			result=first*second;
+			System.out.println("곱셈의 결과 : "+result);
+		} else if(symbol.equals("/")) {
+			result=first/second;
+			System.out.println("나눗셈의 결과 : "+result);
+		} else {
+			System.out.println("사칙연산 기호를 잘못 입력하셨습니다.");
+		} 
+		return result;
+	}
+	static void print(int result) {
+		System.out.println("최종 결과 값 : "+result);
+	}
 
-		 System.out.println("사칙연산 기호를 입력해주세요 :");
-		 String symbol=sc.next();
-		 System.out.println(symbol);
-		 
-		 if("quit".equals(symbol)) {
-			 System.out.println("최종결과 값 : "+result);
-			 break;
-		 }
-		 
-		 System.out.println("두번째 숫자를 입력해주세요");
-		 int num2= sc.nextInt();
-		 System.out.println(num2);
-		 
-		 if(symbol.equals("+")) {
-			 result=result+num2;
-			 System.out.println("덧셈 결과 : "+ result);
-		 } else if(symbol.equals("-")) {
-			 result=result-num2;
-			 System.out.println("뺄셈 결과 : "+ result);
-		 } else if(symbol.equals("*")) {
-			 result=result*num2;
-			 System.out.println("곱셈 결과 : "+ result);
-		 } else if(symbol.equals("/")){
-			 result=result/num2;
-			 System.out.println("나눗셈 결과 :"+result);
-		 } else {
-			 System.out.println("잘못된 기호를 입력하셨습니다.");
-		 }
-	 }
- 	}
- }
+
+public static void main(String[] args) {
+	Scanner sc=new Scanner(System.in);
+	int first=getFirstValue(sc);
+
+	int result=first;
+	while(true) {
+		String symbol=getSymbol(sc);
+		
+	if(symbol.equals("quit")) {
+		print(result);
+		break;
+	}
+	
+	int second=getSecondValut(sc);
+	
+	result=calculate(result, symbol, second);
+	
+	
+		}
+	}
+}
+
